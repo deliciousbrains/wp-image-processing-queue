76bec5ab310b4887d93f5b9bf2d481d17680ec7c
Phil Birnie <me@philbirnie.com>
2018-11-02 16:47:30 -0400

    Fixes PHP Errors caused by invalid Exception Class calls.

--- src/Image_Processing_Queue/Resize_Job.php
+++ src/Image_Processing_Queue/Resize_Job.php
@@ -35,7 +35,7 @@ class Resize_Job extends Job {
         $crop    = $item['crop'];
 
         if ( ! $width && ! $height ) {
-            throw new Exception( "Invalid dimensions '{$width}x{$height}'" );
+            throw new \Exception( "Invalid dimensions '{$width}x{$height}'" );
         }
 
         if ( Queue::does_size_already_exist_for_image( $post_id, array( $width, $height, $crop ) ) ) {
@@ -58,19 +58,19 @@ class Resize_Job extends Job {
         $editor = wp_get_image_editor( $img_path );
 
         if ( is_wp_error( $editor ) ) {
-            throw new Exception( 'Unable to get WP_Image_Editor for file "' . $img_path . '": ' . $editor->get_error_message() . ' (is GD or ImageMagick installed?)' );
+            throw new \Exception( 'Unable to get WP_Image_Editor for file "' . $img_path . '": ' . $editor->get_error_message() . ' (is GD or ImageMagick installed?)' );
         }
 
         $resize = $editor->resize( $width, $height, $crop );
 
         if ( is_wp_error( $resize ) ) {
-            throw new Exception( 'Error resizing image: ' . $resize->get_error_message() );
+            throw new \Exception( 'Error resizing image: ' . $resize->get_error_message() );
         }
 
         $resized_file = $editor->save();
 
         if ( is_wp_error( $resized_file ) ) {
-            throw new Exception( 'Unable to save resized image file: ' . $editor->get_error_message() );
+            throw new \Exception( 'Unable to save resized image file: ' . $editor->get_error_message() );
         }
 
         $size_name = Queue::get_size_name( array( $width, $height, $crop ) );
@@ -80,7 +80,7 @@ class Resize_Job extends Job {
             'height'    => $resized_file['height'],
             'mime-type' => $resized_file['mime-type'],
         );
-        
+
         unset( $image_meta['ipq_locked'] );
         wp_update_attachment_metadata( $post_id, $image_meta );
 	}
